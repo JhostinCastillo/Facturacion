1a6f8374db28f4745fc974ee49c27a632761c2d9
JhostinCastillo <jhostincastilloramirez@gmail.com>
2024-09-13 15:55:49 -0400

    13-9-2024

--- pages/2_🚀_pcb.py
+++ pages/2_🚀_pcb.py
@@ -28,7 +28,7 @@ credentials_module = {
         "access_token": st.secrets["token_response"]["access_token"],
         "expires_in": st.secrets["token_response"]["expires_in"],
         "refresh_token": st.secrets["token_response"]["refresh_token"],
-        "scopes": st.secrets["token_response"]["scopes"],
+        "scope": st.secrets["token_response"]["scope"],
         "token_type": st.secrets["token_response"]["token_type"],
     },
     "scopes": st.secrets["scopes"],
